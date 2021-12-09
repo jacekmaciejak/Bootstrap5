44495eae38ccda8857cb60e63fafb9e189468eb0
Jacek Maciejak <jacekmaciejak@poczta.fm>
2021-12-09 22:28:32 +0100

    Visual changes were made

--- components/gallery_boats.inc.php
+++ components/gallery_boats.inc.php
@@ -2,92 +2,92 @@
     <div class="row">
         <div class="col-4 ">
             <a class="lightbox"
-                href="img/boats/img2.png">
-                <img src="img/boats/img2.png"
+                href="img/boats/img2.PNG">
+                <img src="img/boats/img2.PNG"
                     alt="Boat">
             </a>
         </div>
         <div class="col-4">
             <a class="lightbox"
-                href="img/boats/img3.png">
-                <img src="img/boats/img3.png"
+                href="img/boats/img3.PNG">
+                <img src="img/boats/img3.PNG"
                     alt="Boat">
             </a>
         </div>
         <div class="col-4">
             <a class="lightbox"
-                href="img/boats/img4.png">
-                <img src="img/boats/img4.png"
+                href="img/boats/img4.PNG">
+                <img src="img/boats/img4.PNG"
                     alt="TBoat">
             </a>
         </div>
         <div class="col-8">
             <a class="lightbox"
-                href="img/boats/img1.png">
-                <img src="img/boats/img1.png"
+                href="img/boats/img1.PNG">
+                <img src="img/boats/img1.PNG"
                     alt="Boat">
             </a>
         </div>
         <div class="col-4">
             <a class="lightbox"
-                href="img/boats/img5.png">
-                <img src="img/boats/img5.png"
+                href="img/boats/img5.PNG">
+                <img src="img/boats/img5.PNG"
                     alt="Boat">
             </a>
         </div>
         <div class="col-4">
             <a class="lightbox"
-                href="img/boats/img6.png">
-                <img src="img/boats/img6.png"
+                href="img/boats/img6.PNG">
+                <img src="img/boats/img6.PNG"
                     alt="Boat">
             </a>
         </div>
         <div class="col-4">
             <a class="lightbox"
-                href="img/boats/img7.png">
-                <img src="img/boats/img7.png"
+                href="img/boats/img7.PNG">
+                <img src="img/boats/img7.PNG"
                     alt="Boat">
             </a>
         </div>
         <div class="col-4">
             <a class="lightbox"
-                href="img/boats/img8.png">
-                <img src="img/boats/img8.png"
+                href="img/boats/img8.PNG">
+                <img src="img/boats/img8.PNG"
                     alt="Boat">
             </a>
         </div>
         <div class="col-4">
             <a class="lightbox"
-                href="img/boats/img9.png">
-                <img src="img/boats/img9.png"
+                href="img/boats/img9.PNG">
+                <img src="img/boats/img9.PNG"
                     alt="Boat">
             </a>
         </div>
         <div class="col-4">
             <a class="lightbox"
-                href="img/boats/img10.png">
-                <img src="img/boats/img10.png"
+                href="img/boats/img10.PNG">
+                <img src="img/boats/img10.PNG"
                     alt="Boat">
             </a>
         </div>
         <div class="col-4">
             <a class="lightbox"
-                href="img/boats/img11.png">
-                <img src="img/boats/img11.png"
+                href="img/boats/img11.PNG">
+                <img src="img/boats/img11.PNG"
                     alt="Boat">
             </a>
         </div>
         <div class="col-4">
             <a class="lightbox"
-                href="img/boats/img12.png">
-                <img src="img/boats/img12.png"
+                href="img/boats/img12.PNG">
+                <img src="img/boats/img12.PNG"
                     alt="Boat">
             </a>
         </div>
         <div class="col-4">
             <a class="lightbox"
-                href="img/boats/img13.png">
-                <img src="img/boats/img13.png"
+                href="img/boats/img13.PNG">
+                <img src="img/boats/img13.PNG"
                     alt="Boat">
             </a>
         </div>
--- components/gallery_clocks.inc.php
+++ components/gallery_clocks.inc.php
@@ -2,74 +2,197 @@
     <div class="row">
         <div class="col-4 ">
             <a class="lightbox"
-                href="img/clocks/img2.png">
-                <img src="img/clocks/img2.png"
+                href="img/clocks/img2.PNG">
+                <img src="img/clocks/img2.PNG"
                     alt="Clock">
             </a>
         </div>
         <div class="col-4">
             <a class="lightbox"
-                href="img/clocks/img3.png">
-                <img src="img/clocks/img3.png"
-                    alt=Clock">
+                href="img/clocks/img3.PNG">
+                <img src="img/clocks/img3.PNG"
+                    alt="Clock">
             </a>
         </div>
         <div class="col-4">
             <a class="lightbox"
-                href="img/clocks/img4.png">
-                <img src="img/clocks/img4.png"
+                href="img/clocks/img4.PNG">
+                <img src="img/clocks/img4.PNG"
                     alt="TClock">
             </a>
         </div>
         <div class="col-8">
             <a class="lightbox"
-                href="img/clocks/img1.png">
-                <img src="img/clocks/img1.png"
+                href="img/clocks/img1.PNG">
+                <img src="img/clocks/img1.PNG"
                     alt="Clock">
             </a>
         </div>
         <div class="col-4">
             <a class="lightbox"
-                href="img/clocks/img5.png">
-                <img src="img/clocks/img5.png"
+                href="img/clocks/img5.PNG">
+                <img src="img/clocks/img5.PNG"
                     alt="Clock">
             </a>
         </div>
         <div class="col-4">
             <a class="lightbox"
-                href="img/clocks/img6.png">
-                <img src="img/clocks/img6.png"
+                href="img/clocks/img6.PNG">
+                <img src="img/clocks/img6.PNG"
                     alt="Clock">
             </a>
         </div>
         <div class="col-4">
             <a class="lightbox"
-                href="img/clocks/img7.png">
-                <img src="img/clocks/img7.png"
+                href="img/clocks/img7.PNG">
+                <img src="img/clocks/img7.PNG"
                     alt="Clock">
             </a>
         </div>
         <div class="col-4">
             <a class="lightbox"
-                href="img/clocks/img8.png">
-                <img src="img/clocks/img8.png"
+                href="img/clocks/img8.PNG">
+                <img src="img/clocks/img8.PNG"
                     alt="Clock">
             </a>
         </div>
         <div class="col-4">
             <a class="lightbox"
-                href="img/clocks/img9.png">
-                <img src="img/clocks/img9.png"
+                href="img/clocks/img9.PNG">
+                <img src="img/clocks/img9.PNG"
                     alt="Clock">
             </a>
         </div>
         <div class="col-4">
             <a class="lightbox"
-                href="img/clocks/img10.png">
-                <img src="img/clocks/img10.png"
+                href="img/clocks/img10.PNG">
+                <img src="img/clocks/img10.PNG"
                     alt="Clock">
             </a>
         </div>
     </div>
 
 </div>
+
+
+<!--
+<div class="tz-gallery">
+    <div class="row">
+        <div class="col-4 ">
+            <a class="lightbox"
+                href="img/clocks/img2.PNG">
+                <picture>
+                    <source srcset="img/clocks/img2.webp"
+                        type="image/webp">
+                    <img src="img/clocks/img2.PNG"
+                        alt="Clock">
+                </picture>
+            </a>
+        </div>
+        <div class="col-4">
+            <a class="lightbox"
+                href="img/clocks/img3.PNG">
+                <picture>
+                    <source srcset="img/clocks/img3.webp"
+                        type="image/webp">
+                    <img src="img/clocks/img3.PNG"
+                        alt="Clock">
+                </picture>
+            </a>
+        </div>
+        <div class="col-4">
+            <a class="lightbox"
+                href="img/clocks/img4.PNG">
+                <picture>
+                    <source srcset="img/clocks/img4.webp"
+                        type="image/webp">
+                    <img src="img/clocks/img4.PNG"
+                        alt="Clock">
+                </picture>
+            </a>
+        </div>
+        <div class="col-8">
+            <a class="lightbox"
+                href="img/clocks/img1.PNG">
+                <picture>
+                    <source srcset="img/clocks/img1.webp"
+                        type="image/webp">
+                    <source srcset="img/clocks/img1.PNG"
+                        type="image/PNG">
+                    <img src="img/clocks/img1.PNG"
+                        alt="Clock">
+                </picture>
+            </a>
+        </div>
+        <div class="col-4">
+            <a class="lightbox"
+                href="img/clocks/img5.PNG">
+                <picture>
+                    <source srcset="img/clocks/img5.webp"
+                        type="image/webp">
+                    <img src="img/clocks/img5.PNG"
+                        alt="Clock">
+                </picture>
+            </a>
+        </div>
+        <div class="col-4">
+            <a class="lightbox"
+                href="img/clocks/img6.PNG">
+                <picture>
+                    <source srcset="img/clocks/img6.webp"
+                        type="image/webp">
+                    <img src="img/clocks/img6.PNG"
+                        alt="Clock">
+                </picture>
+            </a>
+        </div>
+        <div class="col-4">
+            <a class="lightbox"
+                href="img/clocks/img7.PNG">
+                <picture>
+                    <source srcset="img/clocks/img7.webp"
+                        type="image/webp">
+                    <img src="img/clocks/img7.PNG"
+                        alt="Clock">
+                </picture>
+            </a>
+        </div>
+        <div class="col-4">
+            <a class="lightbox"
+                href="img/clocks/img8.PNG">
+                <picture>
+                    <source srcset="img/clocks/img8.webp"
+                        type="image/webp">
+                    <img src="img/clocks/img8.PNG"
+                        alt="Clock">
+                </picture>
+            </a>
+        </div>
+        <div class="col-4">
+            <a class="lightbox"
+                href="img/clocks/img9.PNG">
+                <picture>
+                    <source srcset="img/clocks/img9.webp"
+                        type="image/webp">
+                    <img src="img/clocks/img9.PNG"
+                        alt="Clock">
+                </picture>
+            </a>
+        </div>
+        <div class="col-4">
+            <a class="lightbox"
+                href="img/clocks/img10.PNG">
+                <picture>
+                    <source srcset="img/clocks/img10.webp"
+                        type="image/webp">
+                    <img src="img/clocks/img10.PNG"
+                        alt="Clock">
+                </picture>
+            </a>
+        </div>
+    </div>
+
+</div>
+
+
+-->
